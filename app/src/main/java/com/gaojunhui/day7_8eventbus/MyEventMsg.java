4294a3fddb2a7a8d6a4c92406cc8a0c7993090f0
275907983@qq.com <275907983@qq.com>
2016-07-08 18:20:24 +0800

    init

---
+++ app/src/main/java/com/gaojunhui/day7_8eventbus/MyEventMsg.java
@@ -0,0 +1,16 @@
+package com.gaojunhui.day7_8eventbus;
+
+/**
+ * Created by Administrator on 2016/7/8.
+ */
+public class MyEventMsg {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
