53471db03b4fa9e7cd41ea50e1623b9f55d2b9d5
275907983@qq.com <275907983@qq.com>
2016-07-08 18:21:41 +0800

    init

--- app/src/main/java/com/gaojunhui/day7_8eventbus/MainActivity.java
+++ app/src/main/java/com/gaojunhui/day7_8eventbus/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //注册
+        //注册是
         EventBus.getDefault().register(this);
         bt_post= (Button) findViewById(R.id.bt_post);
         bt_post.setOnClickListener(new View.OnClickListener() {
